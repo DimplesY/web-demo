1446f24e3695a30b385e641f98de150fc5ebd345
Dimples_YJ <2890841438@qq.com>
2022-09-21 21:09:51 +0800

    chore: 增加手写防抖、节流函数

--- typescript/tsup-demo/src/function.ts
+++ typescript/tsup-demo/src/function.ts
@@ -3,3 +3,40 @@ function sum(a: number, b: number): number {
 }
 
 export default sum
+
+function debounce(fn: Function, wait: number) {
+  let timer: number
+  return function(){
+    let args = arguments
+    if (timer) clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+    }, wait);
+  }
+}
+
+function throttle1(fn:Function,wait:number){
+  let time: number
+  return function(){
+    let args = arguments
+    let now = Date.now()
+    if(now - time > wait) {
+      fn.apply(this, args)
+    }
+    time = now
+  }
+}
+
+function throttle2(fn:Function, wait:number){
+  let timer:number | null
+  return function(){
+    let args = arguments
+    if(!timer){
+      timer = setTimeout(() => {
+        fn.apply(this, args)
+        timer = null
+      }, wait)
+    }
+  }
+
+}
