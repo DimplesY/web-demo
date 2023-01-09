082ef56932826368b82f990ae0b0f6b72d968c48
Dimples_YJ <2890841438@qq.com>
2023-01-10 00:57:05 +0800

    fix: 删除部分代码

---
+++ design-pattern/代理模式.ts
@@ -0,0 +1,30 @@
+interface Agent {
+  calc(num1: number, num2: number)
+}
+
+
+class NPC1 implements Agent {
+  calc(num1: number, num2: number) {
+    return num1 + num2
+  }
+}
+
+class NPC2 implements Agent {
+  calc(num1: number, num2: number) {
+    return num1 - num2
+  }
+}
+
+
+class Person {
+  agent: Agent
+
+  calc(num1: number, num2: number) {
+   return  this.agent.calc(num1, num2)
+  }
+}
+
+
+let peron = new Person()
+peron.agent = new NPC1()
+console.log(peron.calc(1, 2))
