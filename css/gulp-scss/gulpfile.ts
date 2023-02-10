5d43a39fc1665edce508de1b1c48ec937fa06c0d
Dimples_YJ <2890841438@qq.com>
2023-02-11 00:49:35 +0800

    feat: 增加gulp示例代码

--- css/gulp-scss/gulpfile.ts
+++ css/gulp-scss/gulpfile.ts
@@ -1,13 +1,24 @@
 import dartSass from 'sass'
-import { src, dest } from 'gulp'
+import { src, dest, series } from 'gulp'
 import gulpSass from 'gulp-sass'
+import gulpClean from 'gulp-clean'
+import { resolve, join } from 'path'
 
-export const build = () => {
-  const sass = gulpSass(dartSass)
-  return src('scss/**/*.scss')
-    .pipe(sass.sync())
-    .on('error', sass.logError)
-    .pipe(dest('css'))
+const dist = resolve(__dirname, 'dist')
+const base = resolve(__dirname, 'src')
+
+function clean() {
+  return src(dist, { read: false, allowEmpty: true }).pipe(gulpClean())
 }
 
-export default build
+function buildSass() {
+  const sass = gulpSass(dartSass)
+  return src(join(base, 'styles', '**', '*.scss'))
+    .pipe(sass.sync())
+    .on('error', sass.logError)
+    .pipe(dest(join(dist, 'css')))
+}
+
+export const build = buildSass
+
+export default series(clean, build)
