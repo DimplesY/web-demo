c794ce156e7e095b86a44e611a86b838ec8b78dc
Dimples_YJ <2890841438@qq.com>
2023-07-31 22:40:01 +0800

    feat: 增加面试题demo

---
+++ interview/找零.ts
@@ -0,0 +1,50 @@
+
+interface IRecvice {
+  value: number,
+  money: number
+}
+
+function getRecvice(arr:IRecvice[]) {
+
+    const bitArr = [1,5,10,20,50,100]
+
+    const res = arr.reduce((pre, cur) => pre + cur.money - cur.value, 0)
+
+    const sets = new Set<number>()
+    function dfs(sum:number, nodes: number[]){
+      if(sum === res) {
+        return true
+      }
+
+      if(sum > res || nodes.length === 0) {
+        sets.clear()
+        return false
+      }
+
+      for(let i =0; i< nodes.length ; i++){
+        const current = nodes[i]
+        const newSum = sum + current
+        sets.add(current)
+        const newNodes = nodes.filter(i => !sets.has(i))
+        if(dfs(newSum, newNodes)) {
+          return true
+        }
+      }
+      return false
+    }
+
+    return dfs(0, bitArr)
+}
+
+const result = getRecvice([
+  {
+    value: 0,
+    money: 50
+  },
+  {
+    value: 0,
+    money: 100
+  }
+])
+
+console.log(result)
